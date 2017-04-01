9af23e43d06a31e48aa69a8fcde8d49ed9e39a59
lh120771 <lh120771@alibaba-inc.com>
2017-04-01 22:17:28 +0800

    fix

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ var option = {
         trigger: 'axis'
     },
     legend: {
-        data: shuffle.card.number
+        data: ['0','1','2','3','5','6','7','8','9','10','11','12']
     },
     toolbox: {
         show: true,
